08c7a2c9e010518bea987746c646f96e27cd7926
msjithin <msjithin108@gmail.com>
2024-03-03 23:15:29 -0800

    Module 1 part 1, intro

--- Advanced C++ for Finacial engineering/Readme.md
+++ Advanced C++ for Finacial engineering/Readme.md
@@ -1,3 +1,13 @@
 ## Advanced C++ Design and Implementation in Quantitative Finance
 
 
+Module 1. A simple procedural Monte Carlo  
+Module 2. Introducing objects: basic syntax and design  
+Module 3. Developing the basic structure  
+Module 4. Introducing polymorphism: basic syntax  
+Module 5. A lattice application  
+Module 6. Advanced topics in class design  
+Module 7. Polymorphic I/O  
+Module 8. Generic programming and templates  
+Module 9. Design patterns with objects  
+Module 10. A template factory  
--- Advanced C++ for Finacial engineering/src/main.cpp
+++ Advanced C++ for Finacial engineering/src/main.cpp
@@ -2,8 +2,8 @@
 //  Assessment_v10c_book_NJW
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-#include "Application_Wrapper.h"
-#include "Errorhandler.h"
+#include <iostream>
+#include "utility.h"
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 //  main()
@@ -11,23 +11,25 @@
 
 int main(int argc, char *argv[])
 {
-    ErrorHandler the_handler;
+    // ErrorHandler the_handler;
     
-    try
-    {
-        Application_Wrapper app;    
-        app.run();
-    }
-    catch(const std::runtime_error & e)
-    {
-        the_handler.HandleRunTimeError(e);
-    }
-    catch(...)
-    {
-        the_handler.HandleUnknownError();
-    }
+    // try
+    // {
+    //     Application_Wrapper app;    
+    //     app.run();
+    // }
+    // catch(const std::runtime_error & e)
+    // {
+    //     the_handler.HandleRunTimeError(e);
+    // }
+    // catch(...)
+    // {
+    //     the_handler.HandleUnknownError();
+    // }
                     
-    return the_handler.PauseAndReturn();
+    // return the_handler.PauseAndReturn();
+    std::cout  << "Hello World!" << std::endl;
+    return ut::PauseAndReturn();
 }
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
